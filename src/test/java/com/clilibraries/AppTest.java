66a2405e50c778bbf97758eab5cc6ad0e6d1b475
developeronly <developeronly@gmail.com>
2017-11-05 16:05:57 +0530

    First failing test case for verifying travis

--- src/test/java/com/clilibraries/AppTest.java
+++ src/test/java/com/clilibraries/AppTest.java
@@ -1,5 +1,13 @@
 package com.clilibraries;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class AppTest {
+
+    @Test
+    public void firstFailingTestCase() {
+        Assert.assertTrue(false);
+    }
 
 }

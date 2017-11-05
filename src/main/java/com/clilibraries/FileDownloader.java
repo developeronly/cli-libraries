75ec1cca92a690a05252b7abf0ce5c7021adaadf
developeronly <developeronly@gmail.com>
2017-11-05 17:06:41 +0530

    Added basic url validations.

--- src/main/java/com/clilibraries/FileDownloader.java
+++ src/main/java/com/clilibraries/FileDownloader.java
@@ -1,7 +1,46 @@
 package com.clilibraries;
 
-public class FileDownloader {
-    public FileDownloader(String url, String location) {
+import org.apache.commons.lang3.StringUtils;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FileDownloader {
+
+    private final String url;
+    private final String location;
+
+    public FileDownloader(String url, String location) {
+        if (isUrlBlank(url))
+            throw new IllegalArgumentException("URL can't be empty. Please provide valid URL.");
+        if (isUrlMalformed(url))
+            throw new IllegalArgumentException("Please provide valid URL");
+        this.url = url;
+        this.location = location;
     }
+
+    private boolean isUrlMalformed(String urlOfFile) {
+        if (!isUrlBlank(urlOfFile)) {
+            try {
+                URL url = new URL(urlOfFile);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isUrlBlank(String urlOfFile) {
+        return StringUtils.isBlank(urlOfFile);
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
 }
--- src/test/java/com/clilibraries/FileDownloaderTest.java
+++ src/test/java/com/clilibraries/FileDownloaderTest.java
@@ -1,18 +1,43 @@
 package com.clilibraries;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Objects;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class FileDownloaderTest {
 
     @Test
-    public void fileDownloaderShouldAcceptUrlAndLoactionValues() {
+    public void fileDownloaderShouldAcceptUrlAndLocationValues() {
         String url = "dummy url";
         String location = "dummy location";
         FileDownloader fileDownloader = new FileDownloader(url, location);
-        Assert.assertTrue(Objects.nonNull(fileDownloader));
+        assertTrue(Objects.nonNull(fileDownloader));
+    }
+
+    @Test
+    public void verifyExistenceOfUrlAndLocationValues() {
+        String url = "dummy url";
+        String location = "dummy location";
+        FileDownloader fileDownloader = new FileDownloader(url, location);
+        assertEquals(url, fileDownloader.getUrl());
+        assertEquals(location, fileDownloader.getLocation());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void verifyWhetherUrlIsEmptyOrNot() {
+        String url = "";
+        String location = "dummy location";
+        FileDownloader fileDownloader = new FileDownloader(url, location);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void verifyWhetherUrlIsValidOrNot() {
+        String url = "dummy url";
+        String location = "dummy location";
+        FileDownloader fileDownloader = new FileDownloader(url, location);
     }
 
 

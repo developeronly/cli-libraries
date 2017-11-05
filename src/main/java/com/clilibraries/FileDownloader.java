173b955b1adb9bd43b61a7c123a8da7f2c96ca83
developeronly <developeronly@gmail.com>
2017-11-05 17:33:49 +0530

    Rrefactoring: Extracting out validations.

--- src/main/java/com/clilibraries/FileDownloader.java
+++ src/main/java/com/clilibraries/FileDownloader.java
@@ -12,34 +12,45 @@ public class FileDownloader {
     private final String location;
 
     public FileDownloader(String url, String location) {
-        if (isUrlBlank(url))
-            throw new IllegalArgumentException("URL can't be empty. Please provide valid URL.");
-        if (isUrlMalformed(url))
-            throw new IllegalArgumentException("Please provide valid URL");
-        if (isLocationBlank(location))
-            throw new IllegalArgumentException("File location can't be empty. Please provide location to save file.");
-        if (!isLocationExists(location))
-            throw new IllegalArgumentException("Please provide valid location to save file.");
-
+        isValidParameters(url, location);
         this.url = url;
         this.location = location;
     }
 
-    private boolean isLocationExists(String locationToSaveFile) {
-        File file = new File(locationToSaveFile);
+    private void isValidParameters(String urlToDownloadFileFrom, String locationToSaveDownloadedFie) {
+        validateUrl(urlToDownloadFileFrom);
+        validateLocation(locationToSaveDownloadedFie);
+    }
+
+    private void validateLocation(String locationToSaveDownloadedFie) {
+        if (isLocationBlank(locationToSaveDownloadedFie))
+            throw new IllegalArgumentException("File location can't be empty. Please provide location to save file.");
+        if (!isLocationExists(locationToSaveDownloadedFie))
+            throw new IllegalArgumentException("Please provide valid location to save file.");
+    }
+
+    private void validateUrl(String urlToDownloadFileFrom) {
+        if (isUrlBlank(urlToDownloadFileFrom))
+            throw new IllegalArgumentException("URL can't be empty. Please provide valid URL.");
+        if (isUrlMalformed(urlToDownloadFileFrom))
+            throw new IllegalArgumentException("Please provide valid URL");
+    }
+
+    private boolean isLocationExists(String locationToSaveDownloadedFie) {
+        File file = new File(locationToSaveDownloadedFie);
         if (file.exists()) return true;
         if (file.isDirectory()) return true;
         return false;
     }
 
-    private boolean isLocationBlank(String locationToSaveFile) {
-        return StringUtils.isBlank(locationToSaveFile);
+    private boolean isLocationBlank(String locationToSaveDownloadedFie) {
+        return StringUtils.isBlank(locationToSaveDownloadedFie);
     }
 
-    private boolean isUrlMalformed(String urlOfFile) {
-        if (!isUrlBlank(urlOfFile)) {
+    private boolean isUrlMalformed(String urlToDownloadFileFrom) {
+        if (!isUrlBlank(urlToDownloadFileFrom)) {
             try {
-                URL url = new URL(urlOfFile);
+                URL url = new URL(urlToDownloadFileFrom);
             } catch (MalformedURLException e) {
                 return true;
             }
@@ -47,8 +58,8 @@ public class FileDownloader {
         return false;
     }
 
-    private boolean isUrlBlank(String urlOfFile) {
-        return StringUtils.isBlank(urlOfFile);
+    private boolean isUrlBlank(String urlToDownloadFileFrom) {
+        return StringUtils.isBlank(urlToDownloadFileFrom);
     }
 
     public String getUrl() {

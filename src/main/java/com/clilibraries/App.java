11e567e5a76a8c50146a5bbed76ab19fdff9966c
developeronly <developeronly@gmail.com>
2017-11-05 15:56:53 +0530

    Added basic required libraries and directories.

---
+++ src/main/java/com/clilibraries/App.java
@@ -0,0 +1,7 @@
+package com.clilibraries;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone!");
+    }
+}
---
+++ src/test/java/com/clilibraries/AppTest.java
@@ -0,0 +1,5 @@
+package com.clilibraries;
+
+public class AppTest {
+    
+}
